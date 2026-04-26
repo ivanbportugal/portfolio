29449c30e6fa905db0ca321ae6c853a66069eb32
Ivan Portugal <ivanbportugal@gmail.com>
2026-04-26 14:41:55 -0500

    several enhancements and added 2 years

--- src/config.ts
+++ src/config.ts
@@ -24,106 +24,91 @@ interface SkillIntExpProps {
 
 export const SKILLS: SkillIntExpProps[] = [
   {
-    time: "2008-2010",
-    skills: [
-      {
-        theSkill: "Java",
-        interest: "10",
-        expertise: "3",
-      },
-      {
-        theSkill: "DevOps",
-        interest: "10",
-        expertise: "7",
-      },
-      {
-        theSkill: "C#, .NET",
-        interest: "1",
-        expertise: "0",
-      },
-      {
-        theSkill: "SQL",
-        interest: "3",
-        expertise: "3",
-      },
-      {
-        theSkill: "JavaScript",
-        interest: "8",
-        expertise: "5",
-      },
-    ],
-  },
-  {
-    time: "2011-2014",
-    skills: [
-      {
-        theSkill: "Java",
-        interest: "10",
-        expertise: "7",
-      },
-      {
-        theSkill: "DevOps",
-        interest: "10",
-        expertise: "8",
-      },
-      {
-        theSkill: "C#, .NET",
-        interest: "2",
-        expertise: "1",
-      },
-      {
-        theSkill: "SQL",
-        interest: "4",
-        expertise: "4",
-      },
-      {
-        theSkill: "JavaScript",
-        interest: "10",
-        expertise: "6",
-      },
-      {
-        theSkill: "SPAs",
-        interest: "10",
-        expertise: "5",
-      },
-    ],
-  },
-  {
-    time: "2015-2019",
+    time: "2024-2026",
     skills: [
       {
         theSkill: "Spring Boot",
         interest: "9",
-        expertise: "8",
-      },
-      {
-        theSkill: "DevOps",
-        interest: "9",
         expertise: "9",
       },
       {
-        theSkill: "C#, .NET",
+        theSkill: "Kafka",
         interest: "9",
-        expertise: "6",
+        expertise: "8",
       },
       {
-        theSkill: "NodeJS",
-        interest: "7",
+        theSkill: "Flutter",
+        interest: "10",
+        expertise: "9",
+      },
+      {
+        theSkill: "React, Vue",
+        interest: "10",
         expertise: "8",
       },
       {
         theSkill: "SQL",
         interest: "6",
-        expertise: "6",
+        expertise: "8",
       },
       {
         theSkill: "TypeScript",
         interest: "10",
+        expertise: "9",
+      },
+      {
+        theSkill: "Python",
+        interest: "6",
+        expertise: "7",
+      },
+      {
+        theSkill: "C#, .NET",
+        interest: "2",
+        expertise: "8",
+      },
+    ],
+  },
+  {
+    time: "2022-2024",
+    skills: [
+      {
+        theSkill: "Spring Boot",
+        interest: "9",
+        expertise: "9",
+      },
+      {
+        theSkill: "Kafka",
+        interest: "10",
+        expertise: "7",
+      },
+      {
+        theSkill: "Flutter",
+        interest: "10",
+        expertise: "6",
+      },
+      {
+        theSkill: "React, Vue",
+        interest: "10",
         expertise: "8",
       },
       {
-        theSkill: "SPAs",
+        theSkill: "SQL",
+        interest: "6",
+        expertise: "8",
+      },
+      {
+        theSkill: "TypeScript",
         interest: "10",
+        expertise: "9",
+      },
+      {
+        theSkill: "Python",
+        interest: "9",
+        expertise: "7",
+      },
+      {
+        theSkill: "C#, .NET",
+        interest: "3",
         expertise: "8",
       },
     ],
@@ -174,47 +159,107 @@ export const SKILLS: SkillIntExpProps[] = [
     ],
   },
   {
-    time: "2022-2024",
+    time: "2015-2019",
     skills: [
       {
         theSkill: "Spring Boot",
         interest: "9",
+        expertise: "8",
+      },
+      {
+        theSkill: "DevOps",
+        interest: "9",
         expertise: "9",
       },
       {
-        theSkill: "Kafka",
-        interest: "10",
-        expertise: "7",
-      },
-      {
-        theSkill: "Flutter",
-        interest: "10",
+        theSkill: "C#, .NET",
+        interest: "9",
         expertise: "6",
       },
       {
-        theSkill: "React, Vue",
-        interest: "10",
+        theSkill: "NodeJS",
+        interest: "7",
         expertise: "8",
       },
       {
         theSkill: "SQL",
         interest: "6",
-        expertise: "8",
+        expertise: "6",
       },
       {
         theSkill: "TypeScript",
         interest: "10",
-        expertise: "9",
+        expertise: "8",
       },
       {
-        theSkill: "Python",
-        interest: "9",
+        theSkill: "SPAs",
+        interest: "10",
+        expertise: "8",
+      },
+    ],
+  },
+  {
+    time: "2011-2014",
+    skills: [
+      {
+        theSkill: "Java",
+        interest: "10",
+        expertise: "7",
+      },
+      {
+        theSkill: "DevOps",
+        interest: "10",
+        expertise: "8",
+      },
+      {
+        theSkill: "C#, .NET",
+        interest: "2",
+        expertise: "1",
+      },
+      {
+        theSkill: "SQL",
+        interest: "4",
+        expertise: "4",
+      },
+      {
+        theSkill: "JavaScript",
+        interest: "10",
+        expertise: "6",
+      },
+      {
+        theSkill: "SPAs",
+        interest: "10",
+        expertise: "5",
+      },
+    ],
+  },
+  {
+    time: "2008-2010",
+    skills: [
+      {
+        theSkill: "Java",
+        interest: "10",
+        expertise: "3",
+      },
+      {
+        theSkill: "DevOps",
+        interest: "10",
         expertise: "7",
       },
       {
         theSkill: "C#, .NET",
+        interest: "1",
+        expertise: "0",
+      },
+      {
+        theSkill: "SQL",
         interest: "3",
-        expertise: "8",
+        expertise: "3",
+      },
+      {
+        theSkill: "JavaScript",
+        interest: "8",
+        expertise: "5",
       },
     ],
   },
